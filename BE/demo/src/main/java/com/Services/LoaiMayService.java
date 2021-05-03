7d1224cea1249c2b3136ed61dc22bf6e3273c846
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2021-05-03 11:56:39 +0700

    fix error async

--- BE/demo/src/main/java/com/Services/LoaiMayService.java
+++ BE/demo/src/main/java/com/Services/LoaiMayService.java
@@ -6,7 +6,9 @@ import com.Entity.LoaiMay;
 import com.Repository.LoaiMayRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoaiMayService {
     
     @Autowired
--- e-commerce/src/app/admin/chart/chart.component.ts
+++ e-commerce/src/app/admin/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Chart } from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-chart',
