222fed7b04d7211fc7f31037044ee4130bd1c863
phongphu1802 <65877626+phongphu1802@users.noreply.github.com>
2021-05-03 18:22:56 +0700

    Sửa chart thống kê

--- e-commerce/src/app/admin/chart/chart.component.ts
+++ e-commerce/src/app/admin/chart/chart.component.ts
@@ -8,8 +8,12 @@ import {  ArcElement, BarController, BarElement, BubbleController, CategoryScale
   styleUrls: ['./chart.component.css'],
 })
 export class ChartComponent implements OnInit {
-  canvas: any;
-  ctx: any;
+  canvas1: any;
+  ctx1: any;
+  canvas2: any;
+  ctx2: any;
+  canvas3: any;
+  ctx3: any;
   ngOnInit() {
     Chart.register(
       ArcElement,
@@ -35,13 +39,97 @@ export class ChartComponent implements OnInit {
       Title,
       Tooltip
     );
-    this.canvas = document.getElementById('myChart');
-    this.ctx = this.canvas.getContext('2d');
-    this.ctx = 'myChart';
-    let myChart = new Chart(this.ctx, {
+    //------------------------------------------------------------------------------------------------------------------
+    //Top 10 sản phẩm bán chạy trong tháng
+    this.canvas1 = document.getElementById('myChart1');
+    this.ctx1 = this.canvas1.getContext('2d');
+    this.ctx1 = 'myChart1'
+    let myChart1 = new Chart(this.ctx1, {
       type: 'bar',
       data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    //------------------------------------------------------------------------------------------------------------------
+    //Top 10 sản phẩm hot trong tháng
+    this.canvas2 = document.getElementById('myChart2');
+    this.ctx2 = this.canvas2.getContext('2d');
+    this.ctx2 = 'myChart2'
+    let myChart2 = new Chart(this.ctx2, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    //------------------------------------------------------------------------------------------------------------------
+    //Thống kê doanh thu trong năm
+    this.canvas3 = document.getElementById('myChart3');
+    this.ctx3 = this.canvas3.getContext('2d');
+    this.ctx3 = 'myChart3'
+    let myChart3 = new Chart(this.ctx3, {
+      type: 'bar',
+      data: {
+        labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
         datasets: [
           {
             label: '# of Votes',
